eb93cec67516b440f3f034a471ff06c95fcdf177
feihuan1 <ericrealtor1997@gmail.com>
2024-04-15 21:45:13 -0700

    dynamic secen

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
-
-
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene: PackedScene =preload("res://scenes/projectiles/grenade.tscn")
 
 
 func _on_gate_player_entered_gate(body):
 	print('player entered gate: ', body)
 
 
-func _on_player_laser():
-	print("shoot laser")
-
-
-func _on_player_grenade():
-	print('throw grenade')
-
-
 func _on_gate_player_exited_gate(body):
 	print('player exited gate: ', body)
+
+func _on_player_laser(pos):
+	var laser = laser_scene.instantiate()   
+# 1. update laser position 
+	laser.position = pos
+# 2. move the laser 
+# 3. add laser instance to a Node2D
+	$Projectiles.add_child(laser)
+
+
+func _on_player_grenade(pos):
+	var grenade = grenade_scene.instantiate() 
+	grenade.position = pos 
+	$Projectiles.add_child(grenade)
+	
+
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal laser 
-signal grenade
+signal laser(pos)
+signal grenade(pos)
 
 var can_laser: bool = true
 var can_grenade: bool = true
@@ -15,16 +15,22 @@ func _process(_delta):
 	
 	#laser shooting input 
 #action keep triger when pressed down
-	if Input.is_action_pressed("primary action") and can_laser: 
+	if Input.is_action_pressed("primary action") and can_laser:  
+#ramdomly seclect marker2D for laser
+		var laser_markers = $LaserStartPositions.get_children() 
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false 
 		$Timer.start()
-		laser.emit()
+#emit the position we selected
+		laser.emit(selected_laser.global_position)
 	
 #action JUST triger once
-	if Input.is_action_just_pressed("secondary action") and can_grenade: 
+	if Input.is_action_just_pressed("secondary action") and can_grenade:
+		
 		can_grenade = false	
 		$GrenadeReloadTimer.start() 
-		grenade.emit()
+		var pos = $LaserStartPositions.get_children()[0].global_position
+		grenade.emit(pos)
 
 
 func _on_timer_timeout():
