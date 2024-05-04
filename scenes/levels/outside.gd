66d61b26349575974a3343c81e9d32646b73537d
feihuan1 <ericrealtor1997@gmail.com>
2024-05-04 06:25:11 -0700

    UI

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -4,5 +4,6 @@ extends LevelParent
 
 func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
-	tween.tween_property($Player, "speed", 0, 0.1)
+	tween.tween_property($Player, "speed", 0, 0.3)
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
 
